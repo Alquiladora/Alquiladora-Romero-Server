84bb4045e6566f3453e9e378d60cea68836c7195
edilberto <ing.fera8@gmail.com>
2025-03-09 02:45:00 -0600

    Actualizacion 09/03

--- consultas/clsInventario.js
+++ consultas/clsInventario.js
@@ -8,14 +8,15 @@ const winston = require("winston");
 const crypto = require("crypto");
 const { pool } = require("../connectBd");
 const { csrfProtection } = require("../config/csrf");
+const { getIO } = require("../config/socket");
+const moment = require("moment");
 
 const routerInventario = express.Router();
 routerInventario.use(express.json());
 routerInventario.use(cookieParser());
 
-
 routerInventario.get("/", csrfProtection, async (req, res) => {
-    const sql = `
+  const sql = `
    SELECT 
     p.idProducto,
     p.nombre,
@@ -62,18 +63,161 @@ FROM tblinventario inv
         ON inv.idBodega = bod.idBodega;
 
     `;
-    
+
+  try {
+    const [rows] = await pool.query(sql);
+    res.json(rows);
+  } catch (error) {
+    console.log("Error al obtener inventario: ", error);
+    res.status(500).json({ error: "Error al obtener inventario" });
+  }
+});
+
+routerInventario.put(
+  "/actualizarStock/:idInventario",
+  csrfProtection,
+  async (req, res) => {
+    const { idInventario } = req.params;
+    const { stock } = req.body;
+    const stockToAdd = parseInt(stock, 10);
+    if (isNaN(stockToAdd) || stockToAdd <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "El valor de 'stock' debe ser un número mayor que 0.",
+      });
+    }
     try {
-      const [rows] = await pool.query(sql);
-      res.json(rows);
+      const [rows] = await pool.query(
+        "SELECT stockReal, stock FROM tblinventario WHERE idInventario = ?",
+        [idInventario]
+      );
+
+      if (rows.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "No se encontró el inventario con ese ID.",
+        });
+      }
+
+      const { stockReal, stock: currentStock } = rows[0];
+      const updatedStockReal =
+        stockReal === 0 ? stockToAdd : stockReal + stockToAdd;
+
+      const updatedStock =
+        currentStock === 0 ? stockToAdd : currentStock + stockToAdd;
+      await pool.query(
+        "UPDATE tblinventario SET stockReal = ?, stock = ? WHERE idInventario = ?",
+        [updatedStockReal, updatedStock, idInventario]
+      );
+      return res.json({
+        success: true,
+        message: "Stock actualizado correctamente.",
+        data: {
+          idInventario,
+          stockReal: updatedStockReal,
+          stock: updatedStock,
+        },
+      });
     } catch (error) {
-      winston.error("Error al obtener inventario: ", error);
-      res.status(500).json({ error: "Error al obtener inventario" });
+      console.error("Error al actualizar el stock:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Error interno al actualizar el stock.",
+      });
+    }
+  }
+);
+
+routerInventario.get("/bodegas", async (req, res) => {
+  try {
+    const [rows] = await pool.query(`
+SELECT  idBodega,nombre,es_principal,estado FROM tblbodegas;
+      `);
+    res.status(200).json({ success: true, bodegas: rows });
+  } catch (error) {
+    console.error("Error al obtener bodegas:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error interno del servidor",
+    });
+  }
+});
+
+
+routerInventario.post("/agregar-subbodega", csrfProtection, async (req, res) => {
+    try {
+      const {
+        idProducto,
+        idBodega,
+        stockReal,
+        stock,
+        stockReservado
+      } = req.body;
+  
+    
+      if (
+        !idProducto ||
+        !idBodega ||
+        stockReal === undefined ||
+        stock === undefined ||
+        stockReservado === undefined
+      ) {
+        return res.status(400).json({
+          success: false,
+          message: "Todos los campos son obligatorios."
+        });
+      }
+
+      const checkSql = `
+        SELECT 1 
+        FROM tblinventario
+        WHERE idBodega = ? AND idProducto = ?
+        LIMIT 1
+      `;
+      const [existe] = await pool.query(checkSql, [idBodega, idProducto]);
+      if (existe.length > 0) {
+        return res.status(400).json({
+          success: false,
+          message: "El producto ya existe en esa bodega."
+        });
+      }
+
+      const estado = "activo";
+      const notas = "Registro subbodega";
+       const currentDateTime = moment()
+            .tz("America/Mexico_City")
+            .format("YYYY-MM-DD HH:mm:ss");
+     
+      const sql = `
+        INSERT INTO tblinventario
+        (idBodega, idProducto, stockReal, stock, stockReservado, estado, notas, fechaRegistro)
+        VALUES (?, ?, ?, ?, ?, ?, ?,?)
+      `;
+  
+      // Ejecutamos la inserción
+      await pool.query(sql, [
+        idBodega,
+        idProducto,
+        stockReal,
+        stock,
+        stockReservado,
+        estado,
+        notas,
+        currentDateTime
+      ]);
+  
+      return res.json({
+        success: true,
+        message: "Producto agregado correctamente a la bodega secundaria."
+      });
+    } catch (error) {
+      console.error("Error al insertar producto en subbodega:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Error al insertar producto en subbodega."
+      });
     }
   });
   
 
-
-
-
-module.exports=routerInventario;
+module.exports = routerInventario;
